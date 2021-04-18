03207249800fff1ab662c8341a5c01c65ffb3816
neil.ross@zonedigital.com <neil.ross@zonedigital.com>
2021-04-18 13:05:14 +0100

    add app

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+
+}
+
+export default function App({}: Props): ReactElement {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
